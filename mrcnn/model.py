f9dfab3c01f3a5cabbe28a6d7966ae7fe5033ac0
Sariah <50996677+sariahmghames@users.noreply.github.com>
2023-07-10 14:32:18 +0100

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -4,7 +4,7 @@ The main Mask R-CNN model implementation.
 
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
+Written by Sariah Mghames on top of the version proposed by Waleed Abdulla
 """
 
 import os
